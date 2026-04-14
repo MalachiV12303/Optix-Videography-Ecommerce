30c9a3e174a708f786fe9ee928dc1274afb26b59
MalachiV12303 <malachivalle12303@gmail.com>
2026-04-13 21:01:09 -0400

    style

--- app/ui/store/catalogue/Item.tsx
+++ app/ui/store/catalogue/Item.tsx
@@ -32,7 +32,7 @@ export function Item({
     url: `${process.env.NEXT_PUBLIC_BLOB_BASE_URL}/items/${item.id}.webp`
   };
   return (
-    <div className="group relative h-full flex flex-col gap-6 px-3 py-3 sm:px-6 sm:py-8 border border-foreground-muted bg-background transition">
+    <div className="group relative h-full flex flex-col gap-6 px-2 py-3 sm:px-6 sm:py-8 border border-foreground-muted bg-background transition">
       <Link
         href={`/item?${params}`}
         className="relative self-center w-full md:w-64 aspect-square shrink-0"
